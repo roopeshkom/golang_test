f2c7570ff7781dd2753f2669efe8f4be48450d4a
Roopeswar Kommalapati <roopeshkom@gmail.com>
2018-10-27 18:26:48 -0400

    Added packages to simple test code

---
+++ geometry/rectangle.go
@@ -0,0 +1,16 @@
+package geometry
+
+import "math"
+
+func Rectangle(x, y int) (area, perimeter int, diagonal float64) {
+    area = x*y
+    perimeter = 2*x + 2*y
+    diagonal = math.Sqrt(math.Pow(float64(x), 2) + math.Pow(float64(y), 2))
+    return
+}
+
+func Circle(radius int) (area, circumference float64) {
+    area = math.Pow(float64(radius), 2) * math.Pi
+    circumference = 2 * math.Pi * float64(radius)
+    return
+}
--- test.go
+++ test.go
@@ -1,17 +1,15 @@
 package main
 
 import "fmt"
-import "math"
-
-func rectangle(x, y int) (area, perimeter int, diagonal float64) {
-    area = x*y
-    perimeter = 2*x + 2*y
-    diagonal = math.Sqrt(math.Pow(float64(x), 2) + math.Pow(float64(y), 2))
-    return
-}
+import "test/geometry"
 
 func main() {
     x, y := 10, 12
-    area, _, diagonal := rectangle(x, y)
+    radius := 7
+
+    area, _, diagonal := geometry.Rectangle(x, y)
+    area2, circumference := geometry.Circle(radius)
+
     fmt.Printf("A %vx%v rectangle has an area of %v, and a diagonal of %v.\n", x, y, area, diagonal)
+    fmt.Printf("A circle with radius %v has an area of %v, and a circumference of %v.\n", radius, area2, circumference)
 }
